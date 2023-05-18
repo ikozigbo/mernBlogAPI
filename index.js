4b5f2a1eb3b6f28186adcd5caf3eca8ddae31245
ikozigbo <97337028+ikozigbo@users.noreply.github.com>
2023-05-18 19:07:05 +0100

    18/05

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/Users");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const salt = bcrypt.genSaltSync(10);
+const secret = "gekjirfjmo658493ghdnmewwsmndbn";
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(
-  
-); 
+  "mongodb+srv://ikozigbo:ikponmwosa123@cluster0.b3dt4vh.mongodb.net/?retryWrites=true&w=majority"
+);
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
@@ -23,7 +27,7 @@ app.post("/register", async (req, res) => {
     });
     res.json(userDoc);
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
     res.status(400).json(error);
   }
 });
@@ -31,7 +35,28 @@ app.post("/register", async (req, res) => {
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
-  res.json(userDoc);
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      //logged in
+      jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+        if (err) {
+          throw err;
+        } else {
+          res.cookie("token", token).json("ok");
+        }
+      });
+      //res.json()
+    } else {
+      res.status(400).json("wrong credentials");
+    }
+  } else {
+    res.status(400).json("wrong credentials");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  res.json(req.cookies);
 });
 
 app.listen(4000);
